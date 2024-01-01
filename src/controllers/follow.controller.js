afbbcbc217c7ead33c750dadb29ea3cdca7c45d5
Sukanta Behera <sukantabehera096@gmail.com>
2024-01-01 15:45:43 +0530

    add unfollow endpoint

--- src/controllers/follow.controller.js
+++ src/controllers/follow.controller.js
@@ -47,6 +47,52 @@ const toFollow = asyncHandler(async (req, res) => {
         )
 })
 
+const unFollow = asyncHandler(async(req,res) =>{
+    // get the username from the params
+    // check the user name exist
+    // check is the logged in user follow the user
+    // delete the document in the db
+    const userName = req.params.userName
+
+    if (!userName) {
+        throw new ApiError(402,"User name is missing")
+    }
+
+    const toBeUnfollowedUser = await User.findOne({
+        userName
+    })
+
+    if (!toBeUnfollowedUser) {
+        throw new ApiError(404,"User not found")
+    }
+
+    const follow = await Follow.findOne({
+        $and:[
+            {follower:req.user._id},
+            {toFollowing:toBeUnfollowedUser._id}
+        ]
+    })
+
+    if (!follow) {
+        return res.status(200).json(
+            new ApiResponce(200,{},"User is already unfollowed")
+        )
+    }
+
+    const deletedData = await Follow.deleteOne(
+        {
+            _id:follow._id
+        }
+    )
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponce(200,deletedData,"User unfollowed sucessfully!!!")
+    )
+})
+
 export {
-    toFollow
+    toFollow,
+    unFollow
 }
--- src/routes/follow.routes.js
+++ src/routes/follow.routes.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {
-    toFollow
+    toFollow,
+    unFollow
 } from "../controllers/follow.controller.js"
 
 const router = Router()
 
-router.route("/to-follow/:userName").post(verifyJWT,toFollow)
+router.route("/to-follow/:userName").post(verifyJWT, toFollow)
+router.route("/unfollow/:userName").post(verifyJWT, unFollow)
 
 
 export default router;
