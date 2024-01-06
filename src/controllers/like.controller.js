12560c394ba6692af94fbbcdb780c664b7016885
Sukanta Behera <sukantabehera096@gmail.com>
2024-01-06 10:05:52 +0530

    add end point for unlike the post

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -12,12 +12,26 @@ const likePost = asyncHandler(async (req, res) => {
     }
     const user = req.user;
 
-    const post = await Post.findOne({ _id: postId });
+    const post = await Post.findById(postId);
 
     if (!post) {
         throw new ApiError(404, "Post not found");
     }
 
+    const alreadyLiked = await Like.findOne({
+        $and: [
+            {likedBy: user._id},
+            {likedTo: post._id}
+        ]
+    })
+
+    if (alreadyLiked) {
+        res
+        .status(200)
+        .json(
+            new ApiResponce(200,alreadyLiked,"Post already liked")
+        )
+    }
     const result = await Like.create({
         likedBy: user._id,
         likedTo: post._id,
@@ -32,4 +46,37 @@ const likePost = asyncHandler(async (req, res) => {
         .json(new ApiResponce(200, result, "Post liked successfully"));
 });
 
-export { likePost };
+const unLikePost = asyncHandler(async (req, res) => {
+    const { postId } = req.body;
+
+    if (!postId) {
+        throw new ApiError(402, "Post id is missing");
+    }
+
+    const post = await Post.findById(postId);
+
+    if (!post) {
+        throw new ApiError(404, "Post not found");
+    }
+
+    const alreadyLiked = await Like.findOne({
+        $and: [
+            {likedBy: req.user._id},
+            {likedTo: post._id}
+        ]
+    });
+
+    if (!alreadyLiked) {
+        throw new ApiError(404, "The post is already unliked");
+    }
+
+    const deletedData = await Like.deleteOne({
+        _id: alreadyLiked._id,
+    });
+
+    res.status(200).json(
+        new ApiResponce(200, deletedData, "post unliked sucessfully")
+    );
+});
+
+export { likePost, unLikePost };
--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { likePost } from "../controllers/like.controller.js";
+import { likePost, unLikePost } from "../controllers/like.controller.js";
 
 const router = Router();
 
-router.route("/").post(verifyJWT, likePost);
+router.route("/").post(verifyJWT, likePost).delete(verifyJWT, unLikePost);
 
 export default router;
